ccaffc172179088a41e4a8a00b2a46aba81bd671
김정호 <jeangho293@naver.com>
2024-12-01 01:53:47 +0900

    [fix] context 주입

--- back-office/src/App.tsx
+++ back-office/src/App.tsx
@@ -1,11 +1,13 @@
-import { theme } from '@libs';
+import { AuthProvider, theme } from '@libs';
 import { ThemeProvider } from '@mui/material';
 import { AppRouter } from './routes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
