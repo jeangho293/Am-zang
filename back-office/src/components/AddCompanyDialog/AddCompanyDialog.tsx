83daa828575e0bb7fffd62d636d431e48edf0847
김정호 <jeangho293@naver.com>
2024-12-09 22:18:07 +0900

    [feat] 카카오맵으로 지점 찾기

--- back-office/src/components/AddCompanyDialog/AddCompanyDialog.tsx
+++ back-office/src/components/AddCompanyDialog/AddCompanyDialog.tsx
@@ -1,6 +1,14 @@
-import { Dialog, DialogActions, DialogContent, Stack, TextField, Button } from '@mui/material';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Stack,
+  TextField,
+  Button,
+  InputLabel,
+} from '@mui/material';
 import CompanyIcon from '@assets/company-icon.svg?react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useMutation } from '@libs';
 import { companyRepository } from '@repositories';
 import { useSnackbar } from 'notistack';
@@ -26,12 +34,17 @@ function AddCompanyDialog(props: { onClose: () => void }) {
   });
 
   // 5. form hooks
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, setValue, getValues, control } = useForm({
     mode: 'onChange',
     defaultValues: {
       name: '',
       email: '',
-      address: '',
+      address: {
+        address1: '',
+        address2: '',
+        lat: 0,
+        lng: 0,
+      },
       phoneNumber: '',
     },
   });
@@ -44,34 +57,66 @@ function AddCompanyDialog(props: { onClose: () => void }) {
       <DialogTitleGroup title="Company" icon={<CompanyIcon />} onClose={onClose} />
       <DialogContent>
         <Stack spacing={2} css={{ width: '360px' }}>
-          <TextField {...register('name')} placeholder="name" />
-          <Stack spacing={4} direction="row" css={{ justifyContent: 'space-between' }}>
-            <TextField {...register('address')} placeholder="address" css={{ width: '240px' }} />
-            <DialogButton
-              render={({ onOpen }) => (
-                <Button onClick={onOpen} css={{ height: '32px' }}>
-                  주소 찾기
-                </Button>
-              )}
-            >
-              {({ onClose }) => <SearchMapDialog onClose={onClose} />}
-            </DialogButton>
+          <Stack>
+            <InputLabel>법인 이름</InputLabel>
+            <TextField {...register('name')} />
           </Stack>
-          <TextField {...register('email')} placeholder="email" />
-          <TextField {...register('phoneNumber')} placeholder="phoneNumber" />
+          <Stack spacing={4} direction="row">
+            <Stack css={{ width: '100%' }}>
+              <InputLabel>주소</InputLabel>
+              <Stack direction="row" css={{ justifyContent: 'space-between' }}>
+                <Controller
+                  control={control}
+                  name="address"
+                  render={() => (
+                    <TextField
+                      disabled
+                      value={`${getValues('address.address1')} ${getValues('address.address2')}`}
+                      css={{ width: '240px' }}
+                    />
+                  )}
+                />
+                <DialogButton
+                  render={({ onOpen }) => (
+                    <Button onClick={onOpen} css={{ height: '32px' }}>
+                      주소 찾기
+                    </Button>
+                  )}
+                >
+                  {({ onClose }) => (
+                    <SearchMapDialog
+                      onClose={onClose}
+                      setValue={(address) => {
+                        setValue('address', address);
+                      }}
+                    />
+                  )}
+                </DialogButton>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack>
+            <InputLabel>대표 이메일</InputLabel>
+            <TextField {...register('email')} />
+          </Stack>
+          <Stack>
+            <InputLabel>대표 전화</InputLabel>
+            <TextField {...register('phoneNumber')} />
+          </Stack>
         </Stack>
       </DialogContent>
       <DialogActions>
         <Button
           onClick={handleSubmit(async ({ name, address, email, phoneNumber }) => {
-            await add({
-              variables: {
-                name,
-                email,
-                address,
-                phoneNumber,
-              },
-            });
+            console.log(address);
+            // await add({
+            //   variables: {
+            //     name,
+            //     email,
+            //     address,
+            //     phoneNumber,
+            //   },
+            // });
           })}
         >
           제출
--- back-office/src/components/SearchMapDialog/SearchMapDialog.tsx
+++ back-office/src/components/SearchMapDialog/SearchMapDialog.tsx
@@ -8,35 +8,143 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import { KakaoSearchMap } from '../KakaoSearchMap';
+import { useEffect, useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { DialogTitleGroup } from '../DialogTitleGroup';
 
-function SearchMapDialog(props: { onClose: () => void }) {
+type Marker = {
+  position: {
+    lat: number;
+    lng: number;
+  };
+  placeName: string;
+  address: string;
+};
+
+function SearchMapDialog(props: {
+  onClose: () => void;
+  setValue: ({
+    address1,
+    address2,
+    lng,
+    lat,
+  }: {
+    address1: string;
+    address2: string;
+    lng: number;
+    lat: number;
+  }) => void;
+}) {
   // 1. destructure props
-  const { onClose } = props;
+  const { onClose, setValue } = props;
 
   // 2. lib hooks
   // 3. state hooks
+  const [search, setSearch] = useState('');
+  const [address, setAddress] = useState({
+    address1: '',
+    address2: '',
+    lat: 0,
+    lng: 0,
+  });
+  const [markers, setMarkers] = useState<Marker[]>([]);
+  const [map, setMap] = useState<kakao.maps.Map>();
+
   // 4. query hooks
   // 5. form hooks
   // 6. calculate values
   // 7. effect hooks
+  useEffect(() => {
+    if (search && map) {
+      const ps = new kakao.maps.services.Places();
+
+      ps.keywordSearch(search, (data, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const bounds = new kakao.maps.LatLngBounds();
+          const pins: Marker[] = [];
+
+          const places = data.length > 3 ? data.splice(0, 2) : data;
+          places.forEach((result) => {
+            const lat = Number(result.y);
+            const lng = Number(result.x);
+
+            pins.push({
+              position: {
+                lat,
+                lng,
+              },
+              placeName: result.place_name,
+              address: result.address_name,
+            });
+
+            bounds.extend(new kakao.maps.LatLng(lat, lng));
+          });
+
+          setMarkers(pins);
+          map.setBounds(bounds);
+        }
+      });
+    }
+  }, [search]);
   // 8. handlers
   return (
     <Dialog open>
       <DialogTitleGroup title="Map" onClose={onClose} />
       <DialogContent>
         <Stack spacing={4} css={{ width: '480px' }}>
-          <KakaoSearchMap css={{ width: '100%', height: '240px' }} />
           <Stack spacing={4}>
-            <TextField />
+            <Map
+              center={{
+                lat: 37.566826,
+                lng: 126.9786567,
+              }}
+              level={3}
+              onCreate={setMap}
+              css={{ width: '100%', height: '240px' }}
+            >
+              {markers.map((marker) => (
+                <MapMarker
+                  key={`marker-${marker.position.lat}-${marker.position.lng}-${marker.placeName}`}
+                  position={marker.position}
+                  onClick={() =>
+                    setAddress({
+                      ...address,
+                      address1: marker.address,
+                      lat: marker.position.lat,
+                      lng: marker.position.lng,
+                    })
+                  }
+                >
+                  {marker.placeName}
+                </MapMarker>
+              ))}
+            </Map>
+            <TextField
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  setSearch((event.target as HTMLInputElement).value);
+                }
+              }}
+            />
             <Divider orientation="horizontal" />
             <Typography>결과</Typography>
+            <TextField disabled value={address.address1} />
+            <TextField
+              value={address.address2}
+              onChange={(e) => setAddress({ ...address, address2: e.target.value })}
+            />
           </Stack>
         </Stack>
       </DialogContent>
       <DialogActions>
-        <Button>확인</Button>
+        <Button
+          onClick={() => {
+            setValue({ ...address });
+            onClose();
+          }}
+        >
+          확인
+        </Button>
       </DialogActions>
     </Dialog>
   );
