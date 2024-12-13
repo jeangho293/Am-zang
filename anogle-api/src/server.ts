43e3e1f607075d6dd86df2adaebd6365447060ca
김정호 <jeangho293@naver.com>
2024-12-13 21:31:43 +0900

    AC-1 [feat] uuid handler

--- anogle-api/src/server.ts
+++ anogle-api/src/server.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import { initDatasource } from './databases/mysql';
 import { globalRouter } from './routes';
+import { uuidHandler } from './middlewares';
 
 (async () => {
   await initDatasource();
 
   const app = express();
 
+  app.use(uuidHandler);
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  app.use(cors());
 
   // NOTE: global endpoint Router (Don't touch location!)
   app.use(globalRouter);
