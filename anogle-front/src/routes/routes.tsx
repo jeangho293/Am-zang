cf37568eeaebc6bd8a5733d1d5fe8a97a136b0ea
김정호 <jeangho293@naver.com>
2024-12-13 16:47:32 +0900

    [feat] auth, httpclient, theme

--- anogle-front/src/routes/routes.tsx
+++ anogle-front/src/routes/routes.tsx
@@ -1,5 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SignInScreen } from '@screens';
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { DashboardScreen, SignInScreen } from '@screens';
+import { useUser } from '@libs';
+
+function AuthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
+  const [user] = useUser();
+
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
+  return !user ? <Navigate to="/sign-in" /> : <Outlet />;
+}
+
+function UnAuthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
+  const [user] = useUser();
+
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
+  return !user ? <Outlet /> : <Navigate to="/" />;
+}
 
 export function AppRouter() {
   // 1. destructure props
@@ -13,7 +42,14 @@ export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignInScreen />} />
+        <Route element={<AuthorizedRoute />}>
+          <Route index element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<DashboardScreen />} />
+        </Route>
+
+        <Route element={<UnAuthorizedRoute />}>
+          <Route path="sign-in" element={<SignInScreen />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- anogle-front/src/screens/DashboardScreen/index.ts
+++ anogle-front/src/screens/DashboardScreen/index.ts
@@ -1,2 +1,1 @@
 export * from './DashboardScreen';
-export * from './SignInScreen';
