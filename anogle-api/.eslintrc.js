030aaeaa233d88f037a8040d8a279027014805a4
김정호 <jeangho293@naver.com>
2024-12-13 21:12:50 +0900

    AC-1 [feat] eslint prettier

---
+++ anogle-api/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['anogle-back'],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    '@typescript-eslint/ban-types': 'off',
+  },
+};
