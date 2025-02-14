a96a8e97040be2ce8cc9ba701fd9ff2a20531c7b
김정호 <jeangho293@naver.com>
2025-02-14 17:32:38 +0900

    [feat] kafka를 활용한 이벤트 기반 아키텍쳐

--- anogle-api/src/modules/users/application/users-events.service.ts
+++ anogle-api/src/modules/users/application/users-events.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { DddService } from '@libs/ddd';
 import { EventHandler } from '@libs/decorators';
 import { UserCreatedEvent } from '../domain/events';
+import { getTxId } from '../../../libs/helpers/trace-id';
 
 @Injectable()
 export class UsersEventsService extends DddService {
   @EventHandler(UserCreatedEvent)
-  async on() {
+  async handleUserCreatedEvent(event: UserCreatedEvent) {
+    console.log(event);
     console.log('hi');
   }
 }
