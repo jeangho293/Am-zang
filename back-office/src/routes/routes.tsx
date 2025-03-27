810a44d8c9f3aa4b918b5e16907788a6e4189032
김정호 <jeangho293@naver.com>
2025-03-27 22:25:19 +0900

    [feat] authorized Route

--- back-office/src/routes/routes.tsx
+++ back-office/src/routes/routes.tsx
@@ -1,16 +1,41 @@
 import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { SignInScreen } from '@screens';
 import { useUser } from '@libs/auth';
+import { Stack } from '@mui/material';
+import { Header } from '@components';
 
 function AuthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
   const [user] = useUser();
 
-  return user ? <Outlet /> : <Navigate to="/sign-in" />;
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
+  return user ? (
+    <Stack css={{ width: '100%' }}>
+      <Header />
+      <Outlet />
+    </Stack>
+  ) : (
+    <Navigate to="/sign-in" />
+  );
 }
 
 function UnAuthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
   const [user] = useUser();
 
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
   return user ? <Navigate to="/" /> : <Outlet />;
 }
 
